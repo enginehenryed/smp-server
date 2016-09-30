8332450d538fb2c1131052341fe0cb4d73371a5e
jangjunha <jangjunha113@gmail.com>
2016-09-30 23:23:05 +0900

    Change Spring version to 3.2.17.RELEASE.

--- src/main/java/net/swmaestro/portal/HomeController.java
+++ src/main/java/net/swmaestro/portal/HomeController.java
@@ -1,15 +1,13 @@
 package net.swmaestro.portal;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -19,22 +17,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public @ResponseBody String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+
+	@RequestMapping(value = "/first.json", produces="application/json")
+	public @ResponseBody List<String> firstNames(@RequestParam String term) {
+		List<String> res = new ArrayList<String>();
+		res.add("aa");
+	    return res;
 	}
 	
 }
