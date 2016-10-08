3b39bd71e9db58546b4915d9a32965f2c8b9383b
jangjunha <jangjunha113@gmail.com>
2016-10-08 11:35:26 +0900

    Add cors filter.

---
+++ src/main/java/net/swmaestro/portal/filter/CorsFilter.java
@@ -0,0 +1,23 @@
+package net.swmaestro.portal.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by junha on 2016. 10. 7..
+ */
+public class CorsFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Expose-Headers", "x-requested-with");
+        filterChain.doFilter(request, response);
+    }
+}
