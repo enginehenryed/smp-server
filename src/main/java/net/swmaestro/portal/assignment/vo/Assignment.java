62b23c77b319ae824f1e5c6872e1a59102cf5433
dbgsprw <dbgsprw@gmail.com>
2016-11-19 19:51:41 +0900

    Add attachmentList getter method for Assignment

--- src/main/java/net/swmaestro/portal/assignment/vo/Assignment.java
+++ src/main/java/net/swmaestro/portal/assignment/vo/Assignment.java
@@ -2,8 +2,10 @@ package net.swmaestro.portal.assignment.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
+import net.swmaestro.portal.attachment.vo.Attachment;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -41,6 +43,17 @@ public class Assignment {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern=DATE_PATTERN, timezone = TIME_ZONE)
     private Date assignmentEndAt = null;
+
+    private List<Attachment> attachmentList = null;
+
+    public List<Attachment> getAttachmentList() {
+        return attachmentList;
+    }
+
+    public void setAttachmentList(List<Attachment> attachmentList) {
+        this.attachmentList = attachmentList;
+    }
+
 
     /**
      * Assignment's unique ID.
